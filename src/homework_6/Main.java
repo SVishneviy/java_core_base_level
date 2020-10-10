9d77c3514f9aea7f3c3eddb2e9868527a2a214ce
SVishneviy <onlainer2010@gmail.com>
2020-10-10 21:08:33 +0400

    Homework 6

--- src/homework_6/Main.java
+++ src/homework_6/Main.java
@@ -7,27 +7,18 @@ import homework_6.animals.Dog;
 public class Main {
 
     public static void main(String[] args) {
-        Cat[] cats = {
+        Animal[] animals = {
                 new Cat("Мурзик", "белый", 2, 200, 0),
-                new Cat("Рыжик", "рыжий", 5, 200, 0)
-        };
-
-        Dog[] dogs = {
+                new Cat("Рыжик", "рыжий", 5, 200, 0),
                 new Dog("Бобик", "чёрный", 3, 500, 10),
                 new Dog("Рекс", "коричневый", 6, 500, 10),
                 new Dog("Марли", "белый", 1, 500, 10)
         };
 
-        for (Cat cat : cats) {
-            cat.printInformation();
-            cat.printRun((int) (Math.random() * 210));
-            cat.printSwim((int) (Math.random() * 5));
-        }
-
-        for (Dog dog : dogs) {
-            dog.printInformation();
-            dog.printRun((int) (Math.random() * 510));
-            dog.printSwim((int) (Math.random() * 15));
+        for (Animal animal : animals) {
+            animal.printInformation();
+            animal.printRun((int) (Math.random() * 210));
+            animal.printSwim((int) (Math.random() * 5));
         }
 
         System.out.printf("Всего котов: %d.\n", Cat.getCountCats());
